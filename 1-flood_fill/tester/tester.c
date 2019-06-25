27b324832d4e1d99870e3a8f625046d7bc2ac68e
motaylormo <42254557+motaylormo@users.noreply.github.com>
2019-06-25 13:52:11 -0700

    Create tester.c

---
+++ 1-flood_fill/tester/tester.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tester.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 12:55:40 by mtaylor           #+#    #+#             */
+/*   Updated: 2019/06/25 12:55:41 by mtaylor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "t_point.h"
+
+void	flood_fill(char **tab, t_point size, t_point begin);
+/*
+**	Runs tests from the subject.
+**	(Moulinette tests are locked away in files and inaccessible.)
+*/
+
+static char**	make_area(char** zone, t_point size)
+{
+	char** new;
+
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+	return new;
+}
+
+void	print_map(char **area, t_point size, t_point begin)
+{
+	for (int i = 0; i < size.y; ++i)
+	{
+		for (int j = 0; j < size.x; ++j)
+		{
+			printf(" ");
+			if (i == begin.y && j == begin.x)
+				printf("\e[100;1m");
+			printf((area[i][j] == 'F') ? "\e[37m" : "\e[2m");
+			printf("%c\e[0m", area[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int		matrix_equal(char **a, char **b, t_point size)
+{
+	for (int i = 0; i < size.y; ++i)
+	{
+		if (!(strncmp(a[i], b[i], size.x) == 0))
+			return (0);
+	}
+	return (1);
+}
+void	run_test(char **area, t_point size, t_point begin, char **answer)
+{
+	print_map(area, size, begin);
+	flood_fill(area, size, begin);
+	if (matrix_equal(area, answer, size))
+	{
+		printf("\e[1;32m~Correct~\e[0m\n");
+	}
+	else
+	{
+		printf("\e[1;31m~Incorrect~\e[0m\n");
+
+		printf("should result = \n");
+		print_map(answer, size, begin);
+		printf("your result = \n");
+	}
+	print_map(area, size, begin);
+}
+
+void	subject_tests(void)
+{
+	printf("Example 1:\n");
+	t_point begin = {7, 4};
+	t_point size = {8, 5};
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+	char *answer[] = {
+		"FFFFFFFF",
+		"F000F00F",
+		"F00F000F",
+		"F0FF000F",
+		"FFF0000F"
+	};
+	run_test(make_area(zone, size), size, begin, make_area(answer, size));
+}
+
+int main(void)
+{
+	subject_tests();
+	return (0);
+}
