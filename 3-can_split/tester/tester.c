0d220adbd8f0b966515b9e7808370849ec584d4b
motaylormo <42254557+motaylormo@users.noreply.github.com>
2019-06-25 14:59:07 -0700

    Create tester.c

---
+++ 3-can_split/tester/tester.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tester.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 14:05:14 by mtaylor           #+#    #+#             */
+/*   Updated: 2019/06/25 14:05:15 by mtaylor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+struct s_node 
+{
+	int value;
+	struct s_node *left;
+	struct s_node *right;
+};
+#define N(value, left, right)	&(struct s_node){value, left, right}
+#define L(value)	N(value, NULL, NULL)//leaf node
+
+int		can_split(struct s_node *n);
+
+#define SUBJ_TEST_COUNT 4
+static struct s_node	*g_subj_tests[] = {
+	N(28, N(51, N(26, L(76), L(13)), L(48)), NULL),
+	N(30, N(15, L(61), NULL), N(41, L(80), NULL)),
+	N(10, NULL, L(12)),
+	N(5, L(1), N(6, N(7, L(3), L(2)), N(4, NULL, L(8)))),
+};
+static int g_subj_answers[] = { 1, 0, 1, 0 };
+
+void	run_test(struct s_node *tree, int answer)
+{
+	int	ret = can_split(tree);
+	if (ret == answer)
+		printf("\e[3;32mCorrect\e[0m\n");
+	else
+		printf("\e[3;31mIncorrect\e[0m\n");
+	printf("should return = %d\n", answer);
+	printf("  your return = %d\n", ret);
+}
+
+int main(void)
+{
+	printf("TESTS FROM SUBJECT:\n");
+	for (int i = 0; i < SUBJ_TEST_COUNT; ++i)
+	{
+		printf("\nExample %d: ", i + 1);
+		run_test(g_subj_tests[i], g_subj_answers[i]);
+	}
+	return (0);
+}
