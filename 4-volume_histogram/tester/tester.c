b75c0981818936ef62bfee6654b737cefed44c4d
motaylormo <42254557+motaylormo@users.noreply.github.com>
2019-06-30 12:57:08 -0700

    Update tester.c

--- 4-volume_histogram/tester/tester.c
+++ 4-volume_histogram/tester/tester.c
@@ -5,10 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/22 13:04:43 by mtaylor           #+#    #+#             */
-/*   Updated: 2019/05/22 13:04:44 by mtaylor          ###   ########.fr       */
+/*   Created: 2019/06/30 12:22:58 by mtaylor           #+#    #+#             */
+/*   Updated: 2019/06/30 12:22:58 by mtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <stdio.h>//	printf()
 #include <stdlib.h>//	atoi() & malloc()
@@ -56,9 +57,13 @@ static int	g_moul_answers[] = {
 	0, 0, 1, 0, 17, 343, 211, 26, 46
 };
 
+# define BOLD	"\e[1m"
+# define DIM	"\e[2m"
+# define OFF	"\e[0m"
+
 void	print_array(char *name, int *arr, int size)
 {
-	printf("%s[] = {", name);
+	printf("\t%s[] = {", name);
 	for (int i = 0; i < size; ++i)
 	{
 		printf("%d", arr[i]);
@@ -66,6 +71,7 @@ void	print_array(char *name, int *arr, int size)
 			printf(", ");
 	}
 	printf("};\n");
+	printf("\tsize = %d;\n", size);
 }
 
 int		is_walled(int *arr, int size, int index, int level)
@@ -90,7 +96,8 @@ void	print_diagram(int *arr, int size)
 		if (arr[i] > max)
 			max = arr[i];
 	}
-	for (int level = max; level >= 0; --level)
+	printf(DIM);
+	for (int level = max - 1; level >= 0; --level)
 	{
 		for (int i = 0; i < size; ++i)
 		{
@@ -107,33 +114,46 @@ void	print_diagram(int *arr, int size)
 	}
 	for (int i = 0; i < (size * 2) - 1; ++i)
 		printf("-");
-	printf("\n");
+	printf(OFF "\n");
 }
 
-void	run_tests(int count, int tests_arr[][MAX_ARR_SIZE], int tests_size[], int answers[])
+void	run_test(int tests_arr[MAX_ARR_SIZE], int tests_size, int answer)
 {
-	for (int i = 0; i < count; ++i)
+	int ret = volume_histogram(tests_arr, tests_size);
+	if (ret == answer)
+		printf("\e[3;32mCorrect\e[0m\n");
+	else
+		printf("\e[3;31mIncorrect\e[0m\n");
+	print_array("histogram", tests_arr, tests_size);
+	if (ret == answer)
+		printf("return = %d\n", ret);
+	else
 	{
-		printf("Test %d:\n", i + 1);
-		print_array("histogram", tests_arr[i], tests_size[i]);
-		printf("size = %d;\n", tests_size[i]);
-		print_diagram(tests_arr[i], tests_size[i]);
-		printf("volume_histogram(histogram, size);\n");
-		printf("should return = %d\n", answers[i]);
-		printf("  your return = %d\n", volume_histogram(tests_arr[i], tests_size[i]));
-		if (i + 1 < count)
-			printf("\n");
+		printf("should return = %d\n", answer);
+		printf("  your return = %d\n", ret);
 	}
+	print_diagram(tests_arr, tests_size);
+
+	printf("\n");
 }
 
 int		main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		printf("TESTS FROM SUBJECT:\n");
-		run_tests(SUBJ_TEST_COUNT, g_subj_tests_arr, g_subj_tests_size, g_subj_answers);
-		printf("\nTESTS FROM TRACE:\n");
-		run_tests(MOUL_TEST_COUNT, g_moul_tests_arr, g_moul_tests_size, g_moul_answers);
+		printf(BOLD "TESTS FROM SUBJECT:" OFF "\n");
+		for (int i = 0; i < SUBJ_TEST_COUNT; ++i)
+		{
+			printf(BOLD "Test %d:" OFF " ", i + 1);
+			run_test(g_subj_tests_arr[i], g_subj_tests_size[i], g_subj_answers[i]);
+		}
+		printf("\n");
+		printf(BOLD "TESTS FROM TRACE:" OFF "\n");
+		for (int i = 0; i < MOUL_TEST_COUNT; ++i)
+		{
+			printf(BOLD "Test %d:" OFF " ", i + 1);
+			run_test(g_moul_tests_arr[i], g_moul_tests_size[i], g_moul_answers[i]);
+		}
 	}
 	else
 	{
@@ -141,9 +161,7 @@ int		main(int argc, char **argv)
 		for (int i = 1; i < argc; ++i)
 			arr[i - 1] = atoi(argv[i]);
 		print_array("histogram", arr, argc - 1);
-		printf("size = %d;\n", argc - 1);
+		printf("return = %d\n", volume_histogram(arr, argc - 1));
 		print_diagram(arr, argc - 1);
-		printf("volume_histogram(histogram, size);\n");
-		printf("your return = %d\n", volume_histogram(arr, argc - 1));
 	}
 }
