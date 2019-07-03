94080f1466fadff52215f2d1dadc8acaba561c61
motaylormo <42254557+motaylormo@users.noreply.github.com>
2019-07-02 19:27:41 -0700

    Update longest_sequence.c

--- 2-longest_sequence/mine/longest_sequence.c
+++ 2-longest_sequence/mine/longest_sequence.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/06 12:03:58 by mtaylor           #+#    #+#             */
-/*   Updated: 2019/05/06 12:03:59 by mtaylor          ###   ########.fr       */
+/*   Created: 2019/07/02 19:12:04 by mtaylor           #+#    #+#             */
+/*   Updated: 2019/07/02 19:12:05 by mtaylor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,24 @@ struct s_node
 	struct s_node	*right;
 };
 
-/*
-**	"nodes with consecutive values in increasing order."
-*/
-#define CONSEC(a, b)	(a + 1 == b)
+#define UPDATE_MAX(x, max)	if (x > max) max = x
+#define CONSEC(a, b)	(a + 1 == b)// "nodes with consecutive values in increasing order."
 
-static void	send_snake_down(struct s_node *node, int *max, int seq)
+static void	send_snake_down(struct s_node *n, int *max, int seq)
 {
-	if (node)
+	if (n)
 	{
-		if (seq > *max)
-			*max = seq;
-		if (node->left)
+		seq++;
+		UPDATE_MAX(seq, *max);
+		if (n->left)
 		{
-			send_snake_down(node->left, max,
-					(CONSEC(node->value, node->left->value)) ? seq + 1 : 1);
+			send_snake_down(n->left, max,
+					(CONSEC(n->value, n->left->value)) ? seq : 0);
 		}
-		if (node->right)
+		if (n->right)
 		{
-			send_snake_down(node->right, max,
-					(CONSEC(node->value, node->right->value)) ? seq + 1 : 1);
+			send_snake_down(n->right, max,
+					(CONSEC(n->value, n->right->value)) ? seq : 0);
 		}
 	}
 }
@@ -44,6 +42,6 @@ static void	send_snake_down(struct s_node *node, int *max, int seq)
 int		longest_sequence(struct s_node *node)
 {
 	int	max = 0;
-	send_snake_down(node, &max, 1);
+	send_snake_down(node, &max, 0);
 	return (max);
 }
