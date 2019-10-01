6fb2df590733574a1a787b0e707c7579e7835301
Molly Taylor <mtaylor@e1z3r2p20.42.us.org>
2019-10-01 10:45:05 -0700

    cleaning up clone_list

--- level4/clone_list/mine/clone_list.c
+++ level4/clone_list/mine/clone_list.c
@@ -1,117 +1,61 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   clone_list.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mtaylor <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/12 13:58:14 by mtaylor           #+#    #+#             */
-/*   Updated: 2019/05/12 13:58:16 by mtaylor          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 
 struct s_node {
-	int				data;
-	struct s_node	*next;
-	struct s_node	*other;
+	int           data;
+	struct s_node *next;
+	struct s_node *other;
 };
 
-static int	list_len(struct s_node *n)
+static struct s_node	*fresh_node(int val)
 {
-	int	count = 0;
-	while (n)
-	{
-		count++;
-		n = n->next;
-	}
-	return (count);
+	struct s_node *node = malloc(sizeof(struct s_node));
+	node->data = val;
+	node->next = 0;
+	node->other = 0;
+	return (node);
 }
 
-static struct s_node	*create_node(int value)
+static struct s_node	*add_to_list(struct s_node *list, struct s_node *node)
 {
-	struct s_node	*n;
-
-	n = malloc(sizeof(struct s_node));
-	n->data = value;
-	n->next = NULL;
-	n->other = NULL;
-	return (n);
+	if (!list)
+		return (node);
+	while (list->next)
+		list = list->next;
+	list->next = node;
+	return (node);
 }
 
-/*
-**	Duplicates the list for "data" and "next".
-**	"Other" is always NULL.
-*/
-static struct s_node	*simple_dupe(struct s_node *orig)
+static void		*get_matching_index(struct s_node *orig, struct s_node *ptr,
+									struct s_node *clone)
 {
-	struct s_node	*clone_head = NULL;
-	struct s_node	*clone = NULL;
-
-	while (orig)
+	while (orig && clone)
 	{
-		if (!clone_head)
-		{
-			clone = create_node(orig->data);
-			clone_head = clone;
-		}
-		else
-		{
-			clone->next = create_node(orig->data);
-			clone = clone->next;
-		}
+		if (orig == ptr)
+			return (clone);
 		orig = orig->next;
+		clone = clone->next;
+	}
+	return (0);
+}
+
+struct s_node	*clone_list(struct s_node *orig_head)
+{
+	struct s_node	*clone_head = 0;
+	struct s_node	*clone = 0;
+
+	for (struct s_node *orig = orig_head; orig; orig = orig->next)
+	{
+		clone = add_to_list(clone, fresh_node(orig->data));
+		if (!clone_head)
+			clone_head = clone;
+	}
+
+	clone = clone_head;
+	for (struct s_node *orig = orig_head; orig; orig = orig->next)
+	{
+		if (orig->other)
+			clone->other = get_matching_index(orig_head, orig->other, clone_head);
+		clone = clone->next;
 	}
 	return (clone_head);
-}
-
-/*
-**	"Other... points to any node in the list (another node, itself or NULL)."
-*/
-static void		handle_other(struct s_node *orig_head,
-							struct s_node *clone_head, int len)
-{
-	struct s_node	*orig_arr[len];
-	struct s_node	*clone_arr[len];
-	struct s_node	*orig = orig_head;
-	struct s_node	*clone = clone_head;
-	int	s;
-
-	for (int i = 0; i < len; ++i)
-	{
-		orig_arr[i] = orig;
-		clone_arr[i] = clone;
-		orig = orig->next;
-		clone = clone->next;
-	}
-	orig = orig_head;
-	clone = clone_head;
-	for (int i = 0; i < len; ++i)
-	{
-		if (orig->other != NULL)
-		{
-			s = 0;
-			while ((s < len) && (orig->other != orig_arr[s]))
-				s++;
-			clone->other = clone_arr[s];
-		}
-		orig = orig->next;
-		clone = clone->next;
-	}
-}
-
-/*
-**	"Write a function to create a copy of this list (allocate new memory).
-**	You cannot modify the structure, you cannot modify the list you are given."
-*/
-struct s_node	*clone_list(struct s_node *orig)
-{
-	int				len;
-	struct s_node	*clone;
-
-	len = list_len(orig);
-	clone = simple_dupe(orig);
-	handle_other(orig, clone, len);
-	return (clone);
 }
